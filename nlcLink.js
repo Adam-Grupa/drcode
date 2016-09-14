ed5acc9eff55c38c255ffdde4f7738b4085c16de
el <hylee101001@gmail.com>
2016-09-13 23:07:23 -0400

    nlc link

--- nlcLink.js
+++ nlcLink.js
@@ -1,27 +1,30 @@
-var watson = require('watson-developer-cloud');
-var natural_language_classifier = watson.natural_language_classifier({
-  url: 'https://gateway.watsonplatform.net/natural-language-classifier/api',
-  username: '185ab6a7-fe13-4340-a6c0-ebd9ac0a8b59',
-  password: 'thLctxcVRmwa',
-  version: 'v1'
-});
+var fs  = require("fs");
+var account = fs.readFileSync(__dirname + '/myCred.txt').toString().split('\n');
 
-console.log('NLC Service Logged-in.')
+ var watson = require('watson-developer-cloud');
+ var natural_language_classifier = watson.natural_language_classifier({
+   url: 'https://gateway.watsonplatform.net/natural-language-classifier/api',
+   username: account[0],
+   password: account[1],
+   version: 'v1'
+ });
 
-var fs = require('fs');
-var params = {
-  language: 'en',
-  name: 'My Classifier',
-  training_data: fs.createReadStream(__dirname + '/train.csv')
-};
+ console.log('NLC Service Logged-in.')
 
-console.log('NLC train data inserted.')
+ var fs = require('fs');
+ var params = {
+   language: 'en',
+   name: 'My Classifier',
+   training_data: fs.createReadStream(__dirname + '/train.csv')
+ };
 
-var nlc = natural_language_classifier.create(params, function(err, response) {
-  if (err)
-    console.log(err);
-  else
-    console.log(JSON.stringify(response, null, 2));
-});
+ console.log('NLC train data inserted.')
 
-console.log('NLC obj created.')
+ var nlc = natural_language_classifier.create(params, function(err, response) {
+   if (err)
+     console.log(err);
+   else
+     console.log(JSON.stringify(response, null, 2));
+ });
+
+ console.log('NLC obj created.')
