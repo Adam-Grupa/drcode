4c9a3b5cb9da438517c377b82109c7bf27e09544
Elliot Lee <hylee101001@gmail.com>
2016-10-18 17:54:15 -0400

    dd

--- logic_interface/Drcode.js
+++ logic_interface/Drcode.js
@@ -1,5 +1,5 @@
 var NLCService = require('../nlc/NLCService.js');
-
+var result="";
 
 var method = Drcode.prototype;
 var nlc;
@@ -16,7 +16,11 @@ method.process = function(question, req, res)
   });
   res.write('received the data: ');
   res.write(question + '\n');
-  
+
+  result = "";
+  getCoreVocab(question);
+  res.write('processed data: ');
+  res.write(result + '\n');
   // Define function here
   // Javascript closures allow us to remove unneeded function arguments
   var output = function(response) {
@@ -25,7 +29,7 @@ method.process = function(question, req, res)
     res.write('\n');
 
     // for now, print top three
-    for (var i = 0; i<3; i++) {
+    for (var i = 0; i<4; i++) {
         res.write(rList[i].class_name + '\n');
         res.write(rList[i].confidence + '\n\n');
     }
@@ -35,6 +39,22 @@ method.process = function(question, req, res)
   };
   
   nlc.ask(question, output);
+}
+
+getCoreVocab = function(input)
+{
+  var checkTokens = " to want wanted like liked catch caught feel felt";
+  checkTokens += " there's there a an i me my mine you your yours he his him she her hers they them their thiers";
+  checkTokens += " be am is are was were not and also";
+  checkTokens += " may might will  would can could must have had didn't did don't dont can't cant won't wont";
+  input = input.toLowerCase();
+  var tokenized = input.split(/[ ,]+/);
+
+  for (var i=0; i< tokenized.length; i++)
+  {
+    if (checkTokens.indexOf(tokenized[i]) == -1) result += tokenized[i] + " ";
+    if (tokenized[i]=="can't" || tokenized[i]=="don't") result += "not"+ " ";
+  }
 }
 
 module.exports = Drcode;
